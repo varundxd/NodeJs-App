6edfda1dacebe84f75ac3cb36941f1f67eabe6fe
Divyanshu Srivastava <87711681+varundxd@users.noreply.github.com>
2023-11-25 19:20:27 +0530

    first commit

---
+++ routes/reviews.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true});
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const reviews = require('../controllers/reviews');
+const catchAsync = require('../utils/catchAsync');
+
+router.post('/', isLoggedIn, validateReview, catchAsync( reviews.createReview ));
+
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync( reviews.deleteReview ));
+
+module.exports = router;

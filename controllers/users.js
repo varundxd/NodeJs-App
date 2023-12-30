2d20eec92863d5e13bccedae531612bd5a515782
Divyanshu Srivastava <87711681+varundxd@users.noreply.github.com>
2023-12-30 22:01:51 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -11,7 +11,7 @@ module.exports.register = async (req, res, next) => {
     const registeredUser = await User.register(user, password);
     req.login(registeredUser, (err) => {
       if (err) return next(err);
-      req.flash("success", "Welcome to Yelp Camp!");
+      req.flash("success", "Welcome to Campers World!");
       res.redirect("/campgrounds");
     });
   } catch (e) {
